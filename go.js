35107c7a512755fc0f8afecb736056aaca7ca1b7
huazaizai <1015445413@qq.com>
2018-02-13 20:48:55 +0800

    Add files via upload

---
+++ go.js
@@ -0,0 +1,279 @@
+  import typish from './typewriter.js'
+  import CountUp from 'countup.js'
+  // preload
+  let s1 = require('../images/i-miss-u.png')
+  let s2 = require('../images/lips.svg')
+  let img1 = document.createElement('img')
+  let img2 = document.createElement('img')
+  img1.setAttribute('src', s1)
+  img2.setAttribute('src', s2)
+
+  function go() {
+    typish('#typer')
+      .type('/**', '<span class="note">')
+      .type('0', '<br>')
+      .type('00', '<span class="blank">')
+      .type('今天张先生教你写代码呀', '<span class="note">')
+      .type('0', '<br>')
+      .type('00', '<span class="blank">')
+      .wait(10)
+      .type('等下。。。', '<span class="note">') 
+      .wait(10)
+      .type('么么哒~o(*≧▽≦)ツ', '<span class="note">', 0)
+      .wait(10)
+      .then(() => {
+        memeda()
+       })
+      .wait(25)
+      .type('0', '<br>')
+      .type('*/', '<span class="note">') 
+      .type('0', '<br>')
+      .type('function', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('foreverLove', '<span class="blue">')
+      .type('()', '<span class="gray">')
+      .type('0', '<span class="blank">')
+      .type('{', '<span class="gray">')
+        // 换行
+      .type('0', '<br>')
+      // 两个空格
+      .type('00', '<span class="blank">')
+        // 第一个options
+      .type('let', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('timerOptions', '<span class="gray">')
+      .type('0', '<span class="blank">')
+      .type('=', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type('{', '<span class="gray">')
+      .type('0', '<br>')
+      // 四个空格
+      .type('0000', '<span class="blank">')
+      .type('boy', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'大坏蛋'", '<span class="green">')
+      .wait(15)
+      .del(6)
+      .type("'张文华'", '<span class="green">')
+      .type(',', '<span class="gray">')
+      .type('0', '<br>')
+      // 珊珊
+      .type('0000', '<span class="blank">')
+      .type('girl', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'小仙女'", '<span class="green">')
+      .type(',', '<span class="gray">')
+      .type('0', '<br>')
+      // time
+      .type('0000', '<span class="blank">')
+      .type('start', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'2018-02-14'", '<span class="green">')
+      .type('0', '<br>')
+      .type('00', '<span class="blank">')
+      .type('}', '<span class="gray">')
+      .type('0', '<br>')
+
+        // 第二个options
+      .type('00', '<span class="blank">')
+      .type('let', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('heartOptions', '<span class="gray">')
+      .type('0', '<span class="blank">')
+      .type('=', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type('{', '<span class="gray">')
+      .type('0', '<br>')
+      // 四个空格
+      .type('0000', '<span class="blank">')
+      .type('小仙女喜欢的明星', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'杨幂'", '<span class="green heartBg">')
+      .type(',', '<span class="gray">')
+      .type('0', '<br>')
+      // width
+      .type('0000', '<span class="blank">')
+      .type('小仙女最近喜欢的电视剧', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'谈判官'", '<span class="green">')
+      .type(',', '<span class="gray">')
+      .type('0', '<br>')
+      // margin
+      .type('0000', '<span class="blank">')
+      .type('大坏蛋性格', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'蠢死了啦你'", '<span class="green">')
+      .type(',', '<span class="gray">')
+      .type('0', '<br>')
+      // top
+      .type('0000', '<span class="blank">')
+      .type('小仙女性格', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'一个字：好'", '<span class="green">')
+      .type(',', '<span class="gray">')
+      .type('0', '<br>')
+      // duration
+      .type('0000', '<span class="blank">')
+      .type('小仙女喜欢的东西', '<span class="gray">')
+      .type(':', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type("'大坏蛋也都喜欢，小仙女是他的梦想'", '<span class="green">')
+      .type('0', '<br>')
+      .type('00', '<span class="blank">')
+      .type('}', '<span class="gray">')
+      .type('0', '<br>')
+      
+      // creat new
+      .type('00', '<span class="blank">')
+      .type('let', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('timer', '<span class="red">')
+      .type('0', '<span class="blank">')
+      .type('=', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type('new', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('Timer', '<span class="yellow">')
+      .type('(timerOptions)', '<span class="gray">')
+      .type('0', '<br>')
+      .type('00', '<span class="blank">')
+      .type('let', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('heart', '<span class="red">')
+      .type('0', '<span class="blank">')
+      .type('=', '<span class="blue">')
+      .type('0', '<span class="blank">')
+      .type('new', '<span class="purple">')
+      .type('0', '<span class="blank">')
+      .type('Heart', '<span class="yellow">')
+      .type('(heartOptions)', '<span class="gray">')
+      .type('0', '<br>')
+      // document.body
+      .type('00', '<span class="blank">')
+      .type('document', '<span class="red">')
+      .type('.', '<span class="gray">')
+      .type('body', '<span class="red">')
+      .type('.', '<span class="gray">')
+      .type('appendChild', '<span class="green-blue">')
+      .type('(timer)', '<span class="gray">')
+      .type('0', '<br>')
+      
+      .type('00', '<span class="blank">')
+      .type('document', '<span class="red">')
+      .type('.', '<span class="gray">')
+      .type('body', '<span class="red">')
+      .type('.', '<span class="gray">')
+      .type('appendChild', '<span class="green-blue">')
+      .type('(heart)', '<span class="gray">')
+      .type('0', '<br>')
+      
+      .type('}', '<span class="gray">')
+      .wait(20)
+      .type('0', '<br>')
+      .type('foreverLove', '<span class="blue">')
+      .type('()', '<span class="gray">')
+      .wait(5)
+      .then(() => {
+        transBoard('show', () => {
+          foreverLove()
+        })
+      })
+  }
+  function memeda() {
+    let lip = document.createElement('div')
+    lip.className = 'lip'
+    document.body.appendChild(lip);
+    setTimeout(() => {
+      lip.remove()
+    }, 2500)
+  }
+  function transBoard(type, cb) {
+    let board = document.querySelector('.board')
+    board.style.display = 'block'
+    setTimeout(() => {
+      if (!type || type === 'hide') {
+        board.style.right = 'calc(-100vw - 10px)'
+      } else {
+        board.style.right = '0'
+      }
+    }, 30);
+    setTimeout(() => {
+      cb && cb()
+    }, 2000)
+  }
+  function foreverLove() {
+    let showArea = document.querySelector('#showArea')
+    // let heartBg = document.querySelector('#heartBg')
+    let foot = document.querySelector('.foot')
+    let send = document.querySelector('#send')
+    send.addEventListener('click', () => {
+      stars()
+    })
+    showArea.className = 'show'
+    // heartBg.style.display = 'block'
+    heartMain.style.display = 'block'
+    foot.style.display = 'block'
+    setTimeout(() => {
+      count()
+    }, 2000)
+  }
+  function count() {
+    const date = new Date('2017/07/10 00:00:00')
+    let end = new Date()
+    let seconds = (Date.parse(end) - Date.parse(date)) / 1000
+    let days = Math.floor(seconds / (3600 * 24))
+    let timer = document.querySelector('#timer')
+    timer.style.display = 'block'
+    let numAnim = new CountUp("count", 0, days, 0, 5)
+    numAnim.start()
+  }
+  function stars() {
+    let send = document.querySelector('#send')
+    send.setAttribute('disabled', '')
+    setTimeout(() => {
+      send.removeAttribute('disabled')
+    }, 2000)
+    let max = 5
+    let startsContainer = document.createElement('div')
+    startsContainer.className = 'startsContainer'
+    for (let i = 0; i < 30; i++) {
+      let img = document.createElement('img')
+      let src = require('../images/i-miss-u.png')
+      img.setAttribute('src', src)
+      img.setAttribute('class', 'stars')
+      startsContainer.appendChild(img)
+    }
+    let comments = document.querySelectorAll('.comment')
+    let comentsLength = 0
+    if (comments) {
+      comentsLength = comments.length
+      if (comentsLength > 7) {
+        for(let i = 0; i < comentsLength; i++) {
+          comments[i].remove()
+        }
+        comentsLength = 0
+      }
+    }
+    let comment = document.createElement('div')
+    comment.className = 'comment'
+    comment.style.bottom = 80 + comentsLength * 55 + 'px'
+    comment.innerHTML = '我想你了'
+    document.body.appendChild(comment)
+    
+    document.body.appendChild(startsContainer)
+    let els = document.querySelectorAll('.startsContainer')
+    let current = els.length
+    if (current > max) {
+      els[0].remove()
+    }
+  }
+  export {
+    go
+  }
